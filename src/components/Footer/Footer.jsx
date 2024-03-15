2f6b6767dda6b914a6cb4b853506b921cc35ba29
DanielLasso <144230656+Daniellasso@users.noreply.github.com>
2024-03-15 07:44:01 -0300

    .

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,27 @@
 import { StyleSheet, Text, View, ScrollView } from "react-native";
 import React from "react";
 import Shoes from "../Shoes/Shoes";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Footer() {
+  const navigation = useNavigation();
   return (
     <View>
       <Text style={styles.title}>VOCÊ TAMBÉM PODE GOSTAR</Text>
       <View style={{ flexDirection: "row" }}>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
           <View style={{ marginHorizontal: 10 }}>
-            <Shoes img={require("../../assets/1.png")} cost="110,90">
+            <Shoes img={require("../../assets/1.png")} cost="110,90" onClick={() => navigation.navigate("Detail")}>
               Nike Air Max Dia
             </Shoes>
           </View>
           <View style={{ marginHorizontal: 10 }}>
-            <Shoes img={require("../../assets/5.png")} cost="360,90">
+            <Shoes img={require("../../assets/5.png")} cost="360,90" onClick={() => navigation.navigate("Produto5")}>
               Nike Epic React Flyknit 2
             </Shoes>
           </View>
           <View style={{ marginHorizontal: 10 }}>
-            <Shoes img={require("../../assets/3.png")} cost="890">
+            <Shoes img={require("../../assets/3.png")} cost="890" onClick={() => navigation.navigate("Produto3")}>
               Nike Squidward Tentacles
             </Shoes>
           </View>
